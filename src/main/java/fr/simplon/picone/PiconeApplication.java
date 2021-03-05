0bacc012406d7cbf18ce7ecd635904f4084c6525
Kevin DvZ <kevin.deveza@gmail.com>
2021-03-05 13:35:40 +0100

    first commit

---
+++ src/main/java/fr/simplon/picone/PiconeApplication.java
@@ -0,0 +1,13 @@
+package fr.simplon.picone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiconeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiconeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/simplon/picone/PiconeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.simplon.picone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiconeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
