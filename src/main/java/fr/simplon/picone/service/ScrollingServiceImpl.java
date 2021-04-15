e2232261fca55ea5f8a6a39b44b69df583181b4f
Kevin DvZ <kevin.deveza@gmail.com>
2021-04-15 17:23:36 +0200

    test unitaires repository : prblm init

--- src/main/java/fr/simplon/picone/service/ScrollingServiceImpl.java
+++ src/main/java/fr/simplon/picone/service/ScrollingServiceImpl.java
@@ -3,13 +3,15 @@ package fr.simplon.picone.service;
 import fr.simplon.picone.model.Scrolling;
 import fr.simplon.picone.repository.ScrollingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ScrollingServiceImpl implements ScrollingService{
+public class ScrollingServiceImpl implements ScrollingService {
 
 
     @Autowired
@@ -40,5 +42,40 @@ public class ScrollingServiceImpl implements ScrollingService{
 
         monScro.map(this.scrollingRepository::save);
         return scrollingRepository.findById(id);
+    }
+
+    @Override
+    public ResponseEntity deleteScrolling(Long id) {
+        if(!scrollingRepository.existsById(id)){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Le noeud a supprimer n'existe pas. ");
+        } else {
+            int sum = 0;
+            scrollingRepository.deleteById(id);
+            while (scrollingRepository.existsById(id) && sum < 5) {
+                try {
+                    Thread.sleep(1000);
+                    sum++;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (!scrollingRepository.existsById(id)) {
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body("Le noeud a ete supprime");
+            } else {
+                return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
+                        .body("La connexion avec le serveur n'a pas pu être établie correctement.");
+            }
+        }
+
+
+
+    }
+
+    @Override
+    public Scrolling createIsolatedScrolling(Scrolling scrolling) {
+        return scrollingRepository.save(scrolling);
     }
 }
