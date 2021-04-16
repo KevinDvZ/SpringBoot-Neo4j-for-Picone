0c91a58f6913be715977a596754f70ce2a88e1d3
Kevin DvZ <kevin.deveza@gmail.com>
2021-04-16 17:12:03 +0200

    test unitaires repository : essais Container , suite

--- src/test/java/fr/simplon/picone/repository/ScrollingRepositoryTest.java
+++ src/test/java/fr/simplon/picone/repository/ScrollingRepositoryTest.java
@@ -1,77 +1,38 @@
 package fr.simplon.picone.repository;
-import fr.simplon.picone.model.Scrolling;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
+import fr.simplon.picone.model.Scrolling;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.neo4j.ogm.config.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.neo4j.AutoConfigureDataNeo4j;
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @DataNeo4jTest
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 public class ScrollingRepositoryTest {
 
     @Container
-    public static Neo4jContainer neo4jContainer = new Neo4jContainer("neo4j");
+    public static Neo4jContainer neo4jContainer = new Neo4jContainer("neo4j")
+            .withoutAuthentication();
 
 
-    @TestConfiguration
-    static class Config {
-        @Bean
-        public org.neo4j.ogm.config.Configuration configuration() {
-            return new Configuration.Builder()
-                    .uri(neo4jContainer.getBoltUrl())
-                    .credentials("neo4j", neo4jContainer.getAdminPassword())
-                    .build();
-        }
+    @DynamicPropertySource
+    static void neo4jProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.neo4j.uri", neo4jContainer::getBoltUrl);
     }
 
+
     @Autowired
-    private ScrollingRepository scrollingRepository;
+    private ScrollingRepository exampleRepository;
 
-
-    public List<Scrolling> testSaveMethod() {
+   public List<Scrolling> testSaveMethod() {
 
         List<Scrolling> inputScrolling = new ArrayList<>();
         inputScrolling.add(new Scrolling(33L, true, true, 100L, "bec8a3"));
@@ -83,21 +44,9 @@ public class ScrollingRepositoryTest {
     }
 
 
-    @Test
-    public void testActivationContainer() {
+@Test
+   public void contextLoads() {
+       System.out.println("Context Load!");
+        System.out.println(neo4jContainer.getBoltUrl());}
 
-        assertTrue(neo4jContainer.isRunning());
-        System.out.println(neo4jContainer.getBoltUrl());
-        /*
-        //GIVEN
-        this.scrollingRepository.saveAll(testSaveMethod());
-
-        //WHEN
-        List<Scrolling> scrollingAll = scrollingRepository.findAll();
-
-        //THEN
-        assertThat(scrollingAll.size()).isEqualTo(1);
-        */
-
-    }
 }
